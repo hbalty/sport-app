d65d52ef56f7170aa1d272b187423104095ba80a
Houssem Balty <balty.houssem@gmail.com>
2022-04-07 15:53:05 +0200

    created basic UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { TeamComponent } from './team/team.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent
+  },
+  {
+    path: 'team/:teamId',
+    component: TeamComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
